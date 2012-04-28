53632c6f67db71753239156aaa7483ce7f0a36ef
unknown <oilart@oilart-PC.(none)>
2012-04-28 23:48:13 +0200

    data loader

--- DartReader.dart
+++ DartReader.dart
@@ -1,51 +1,47 @@
 #import('dart:html');
 #import('dart:uri');
-#import('dart:io');
 #import('dart:json');
 #source ('client/templates/Source_list.dart');
 
 class DartReader {
-  final SOURCE_FEED_URL="http://127.0.0.1:8080/";
+  final SOURCE_FEED_URL="http://127.0.0.1:8080/feeds";
   
   DartReader() {
     
   }
-
+  
   void run() {
+    window.on.message.add(dataReceived);
     init_source();
   }
 
   void init_source(){
     List source_list = get_fake_source_list();
-
+     
+    loadSourceData(SOURCE_FEED_URL);
+  }
+ 
+  
+  void display_source_list(List source_list){
     SourceList source_tmp = new SourceList(source_list);
     document.body.elements.add(source_tmp.root);
-    
-    loadSourceData(SOURCE_FEED_URL);
   }
   
   List get_fake_source_list(){
     List list = ['cnn', 'bbc'];
     return list;
   }
+
+  //data loaders
+  void dataReceived(MessageEvent e) {
+    var data = JSON.parse(e.data);
+    print(data['responseData']);
+  }
   
-  void loadSourceData(String feedURL)
-  {
-    XMLHttpRequest req =
-        new XMLHttpRequest.getTEMPNAME(feedURL,
-               (result) {
-                   List posts = JSON.parse(result.responseText);
-               });   
-    
-    //HttpClient httpClient = new HttpClient();
-    
-    //HttpClientConnection conn = httpClient.getUrl(new Uri.fromString(feedURL));
-    //conn.onResponse =
-    //  (HttpClientResponse resp)
-    //  {
-    //    Object data = JSON.parse(resp.inputStream.read().toString());
-    //  };
-    //httpClient.shutdown();
+  void loadSourceData(String feedURL){
+    Element script = new Element.tag("script");
+    script.src = feedURL;
+    document.body.elements.add(script);
   }
 }
 
