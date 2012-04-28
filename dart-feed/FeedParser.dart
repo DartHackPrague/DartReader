80b0964906763983a3ddd47b2b4319567e6b53c9
Alexander Dolin <alex.dolin@gmail.com>
2012-04-28 16:14:41 +0200

    Work on feed parser

--- dart-feed/FeedParser.dart
+++ dart-feed/FeedParser.dart
@@ -2,6 +2,7 @@
 
 #import('dart:io');
 #import('dart:uri');
+#import('../3rdParty/dart-xml/Xml.dart');
 
 #source('Feed.dart');
 #source('FeedItem.dart');
@@ -20,6 +21,18 @@ class FeedParser
       (HttpClientResponse resp)
       {
         List content = resp.inputStream.read();
+        String xmlString = new String.fromCharCodes(content);
+        XmlElement xml = XML.parse(xmlString);
+        
+        Feed f = new Feed();
+        f.feedItems = new List<FeedItem>();
+        for (var item in xml.query('item'))
+        {
+          FeedItem fi = new FeedItem();
+          fi.title = "test";
+        }
+        
+        completer.complete(f);
       };
     httpClient.shutdown();
     
